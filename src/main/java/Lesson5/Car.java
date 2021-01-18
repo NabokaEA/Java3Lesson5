5f4c18106a2fac200bb52fe01721d72751396de3
NabokaEA <it_str@amaks-hotels.ru>
2021-01-18 19:58:53 +0200

    полпути

--- src/main/java/Lesson5/Car.java
+++ src/main/java/Lesson5/Car.java
@@ -1,22 +1,26 @@
 package Lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable{
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
     private Race race;
-    private int speed;
     private String name;
+    private int speed;
+    private CyclicBarrier cyclicBarrier;
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier1) {
         this.race = race;
         this.speed = speed;
+        this.cyclicBarrier=cyclicBarrier1;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -26,6 +30,9 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+            Thread.sleep(2);
+            System.out.println("Поток " + this.name + " запустился");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/Lesson5/Tunnel.java
+++ src/main/java/Lesson5/Tunnel.java
@@ -1,20 +1,26 @@
 package Lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private Semaphore semaphore;
+    public Tunnel(Semaphore sima) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        this.semaphore=sima;
     }
     @Override
     public void go(Car c) {
         try {
             try {
+                semaphore.acquire();
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
+                semaphore.release();
                 System.out.println(c.getName() + " закончил этап: " + description);
             }
         } catch (Exception e) {
